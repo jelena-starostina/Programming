4bcf12eb19992fb198ec73b2bf2f1ca71329b5e0
Jelena Starostina <jelena.starostina@ivkhk.ee>
2025-10-10 18:35:38 +0300

    sort.js

--- sort.js
+++ sort.js
@@ -38,3 +38,58 @@ const countVowels = (str) => {
 };
 const sortedByVowelCount = [...countries].sort((a,b) => countVowels(a) - countVowels(b));
 console.log("5. Sorted by number of vowels (fewest to most):", sortedByVowelCount);
+
+//6. Случайная сортировка
+//6. Randon sorting
+const randomSort = [...countries].sort(() => Math.random() - 0,5);
+console.log("6. Randomly sorted:", randomSort);
+
+//Классические алгоритмы сортировки
+//classic sorting algorithms
+
+//1. Пузырьковая сортировка (Bubble Sort)
+//1. Bubble Sort
+//Идея: многократно проходить по массиву, сравнивая соседние элементы
+//и меняя их местами, если они в неправильном порядке.
+//O(n^2) - квадратная сложность
+//Очень медленная для больших массивов
+
+const bubbleSort = (arr) => {
+  const newArr = [...arr];
+  const n = newArr.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n -1; j++) {
+      if (newArr[j].localeCompare(newArr[j + 1]) > 0) {
+        //меняем элементы местами
+        [newArr[j], newArr[j + 1]] = [newArr[j + 1], newArr[j]];
+      }
+    }
+  }
+  return newArr;
+}
+console.log("Bubble sorted:", bubbleSort(countries));
+
+
+//2. Сортировка выбором (Selection Sort)
+//2. Selection Sort
+//Идея: находить наименьший (или наибольший) элимент в массиве
+//и помещать его в начало (или конец) отсортированной части массива.
+//O(n^2) - квадратичная сложность
+//Тоже медленная для больших массивов
+
+//3. Сортировка вставками (Insertion Sort)
+//3. Insertion Sort
+//Идея: строить отсортированный массив по одному элементу,
+//вставляя каждый новый элемент в его правильное место.
+//O(n^2) - квадратичная сложность
+//эффективна  для небольших массивов или почти отсортированных данных
+
+//4.Bogo Sort (Шутка)
+//4.Bogo Sort (joke Sort)
+//Идея:Случайным образом перемешивать массив, пока он не будет отсортирован.
+//O((n+1)!) - факторная сложность
+//очень неэффективный и нереалистичный алгоритм, используемый только в образовательных целях.
+
+
+
